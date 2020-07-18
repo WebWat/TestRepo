09d42e2ebe0ab507943e98773215d6aa28d9ca6c
Serega <seregaplyaskin8@gmail.com>
2020-07-18 21:58:24 +0900

    Add async

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello1!");
-            Console.WriteLine("Hello2!");
-            Console.WriteLine("Hello3!");
-            Console.WriteLine("Hello4!");
-            Console.WriteLine("Hello4!");
-            Console.WriteLine("Hello5!");
+            for (int i = 0; i <= 10; i++)
+            {
+                await Task.Run(() => Console.WriteLine($"Hello{i}!"));
+                await Task.Delay(200);
+            }
+            Console.WriteLine("The End");
+            Console.ReadKey();
         }
     }
 }
