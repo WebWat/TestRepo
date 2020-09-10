4c86510463f47e4fd5f35b9384b6b7dfb1f6a91c
Serega <seregaplyaskin8@gmail.com>
2020-09-10 19:51:49 +0900

    Removed ReadProcessMemory from Kernel32

--- src/ConsoleApp1/Memory/Kernel32.cs
+++ src/ConsoleApp1/Memory/Kernel32.cs
@@ -8,8 +8,5 @@ namespace ConsoleApp1.Memory
         [return: MarshalAs(UnmanagedType.Bool)]
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern bool GlobalMemoryStatusEx([In, Out] MEMORYSTATUSEX lpBuffer);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out, MarshalAs(UnmanagedType.AsAny)] object lpBuffer, int dwSize, out int lpNumberOfBytesRead);
     }
 }
